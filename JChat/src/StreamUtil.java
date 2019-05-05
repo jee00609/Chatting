54c9f1faf5ef5d52cc79721643af3be728df47ce
jee00609 <31675804+jee00609@users.noreply.github.com>
2019-05-05 18:48:29 +0900

    Add files via upload

---
+++ JChat/src/StreamUtil.java
@@ -0,0 +1,13 @@
+import java.io.*; 
+
+public class StreamUtil {
+     public static void close(Closeable out) {
+         if (out != null) {
+             try { 
+                out.close(); 
+            } catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+    } 
+}
